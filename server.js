aa93ede880b2a15f1288ce21684378f7594acfdb
Dushime Eric <devericastro@gmail.com>
2025-04-29 14:49:33 +0200

    commit

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+app.options('*', cors());
 
 const PORT = process.env.PORT || 'https://nx-backend-topaz.vercel.app';
 
